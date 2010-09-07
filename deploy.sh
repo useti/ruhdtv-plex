ff903af35bfef7ea6ac3d22fae5d52a3fa197de1
Grigory Bakunov <thebobuk@ya.ru>
2010-09-07 05:09:58 +0400

    fix dummy error

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-B="$HOME/Library/Application Support/Plex Media Server/Plug-ins/RuHDtv.bundle/Contents"
+B="$HOME/Library/Application Support/Plex Media Server/Plug-ins/RuHDtv.bundle/"
 mkdir -p "$B"
 cp -r "Contents" "$B"
 echo "Done. God bless git and Linus."
